299db8f59ca0e78265319b1db37b9357e9b3c239
Ryan Freckleton <ryan.freckleton@gmail.com>
2017-06-27 15:22:29 -0600

    Add console_scripts entry point

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
 setup(name="hello",
-      version='0.0.1',
-      packages=["hello"])
+      version='0.0.2',
+      packages=["hello"],
+      entry_points = {
+          'console_scripts': ['hello=hello:hello']
+      }
+      )
