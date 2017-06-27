9c711fea0402539bdd928459499e7adcff809a38
Ryan Freckleton <ryan.freckleton@gmail.com>
2017-06-27 15:35:53 -0600

    Adding more metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup
 
 setup(name="hello",
-      version='0.0.3',
+      version='0.0.4',
+      description="Say high to the world",
+      long_description="A demonstration of python packaging with setup.py",
+      classifiers=[
+          'Development Status :: 3 - ALpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Text Processing :: Linguistic'
+      ],
+      keywords='tutorial pysprings color',
+      url="http://meetup.com/pysprings",
+      author="PySprings",
+      author_email="pysprings@pysprings.com",
+      license="MIT",
       packages=["hello"],
       entry_points = {
           'console_scripts': ['hello=hello:hello']
