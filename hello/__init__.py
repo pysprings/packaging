d9f0e62b478db9048c12d29d38b1dc6eb403ca64
Ryan Freckleton <ryan.freckleton@gmail.com>
2017-06-27 15:29:54 -0600

    Adding color and external dependency

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,5 +1,6 @@
+from colorama import init, Fore, Back, Style
 def hello():
-    print("Hello, World!")
+    print(Fore.GREEN + "Hello, World!" + Style.RESET_ALL)
 
 if __name__ == '__main__':
     hello()
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name="hello",
-      version='0.0.2',
+      version='0.0.3',
       packages=["hello"],
       entry_points = {
           'console_scripts': ['hello=hello:hello']
-      }
+      },
+      install_requires=['colorama']
       )
